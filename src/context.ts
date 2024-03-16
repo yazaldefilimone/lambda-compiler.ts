eed819a4bb63c5386978fc72e1ccf697c8c7fe0d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-16 23:21:25 +0200

    *

---
+++ src/context.ts
@@ -0,0 +1,4 @@
+import { Type } from "types";
+
+export const contextTabeleTypes:Map<string, Type> = new Map<string, Type>();
+export const contextTabeleNormalize:Map<string, Type> = new Map<string, Type>();
