55161f1bab6ae618879cd62da8b32444c76c1b84
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-17 19:36:53 +0200

    *

--- src/core.ts
+++ src/core.ts
@@ -2,6 +2,7 @@ import { tokenizer } from "tokenizer"
 import { parse } from "parse";
 import { normalizer } from "normalizer";
 import { checker } from "checker";
+import { compute } from "compute";
 
 function main() {
   const test = `λx. (x x) λx. (x x)`
@@ -15,12 +16,15 @@ function main() {
   const typedFour = `λx:Bool. (λy:Bool. (λz:Bool. (λw:Bool. (x (y (z w))))))`
   const typeFn =  `λx:(Bool -> a). (x x) λx:Bool. (x x)`
   const typeFn2 =  `λx:(Bool -> Bool). (λy:Bool. (x y))`
-
   const complex = `
   (λx: ((Bool -> a) -> (Float -> b) -> c).
   λy: (Bool -> a).
   λz: (Float -> b).
     (x y z) False)
+  `
+
+  const simpleLambTyped = `
+  (λx: A.(λy: B.x y) z)
   `
 
 const long = `
@@ -31,17 +35,22 @@ ${three}
 ${four}
 `
   // const result = tokenizer(four).getTokens();
-  const lex = tokenizer(complex)
+  const lex = tokenizer(two)
   // console.log("Simple Lambda Calculus:")
   // console.log(result);
   // console.log("--------------------------------------------------")
   // console.log("Typed Lambda Calculus:")
   // console.log(typedResult);
+  console.log(simpleLambTyped.trim())
   const program = parse(lex);
+  // console.log(JSON.stringify(program, null, 2));
   const checked = checker(program).bind();
-  console.log(JSON.stringify(checked, null, 2));
-  // const normalized = normalizer(checked);
+  // console.log(JSON.stringify(checked, null, 2));
+  console.log("----- normalize: ---------")
+  const normalized = normalizer(checked).normalize();
   // console.log(JSON.stringify(normalized, null, 2));
+  const computed  = compute(normalized).computed()
+  console.log(computed)
   // console.log(JSON.stringify(program, null, 2));
 
 }
