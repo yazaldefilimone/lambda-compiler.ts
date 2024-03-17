9093c4574e5229bde44d1ef40e8448e2ea7154cc
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-03-17 19:36:53 +0200

    *

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,6 @@
 import { Type } from "types";
 
 export const contextTabeleTypes:Map<string, Type> = new Map<string, Type>();
+export type ContextTabeleTypes = typeof contextTabeleTypes;
 export const contextTabeleNormalize:Map<string, Type> = new Map<string, Type>();
+export type ContextTabeleNormalize = typeof contextTabeleNormalize;
